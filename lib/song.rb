ccb33be8b699436f711d797d4ca63f9d1b506dc5
Keillan Cruickshank <cruickshankxkeillan90@gmail.com>
2019-10-31 13:29:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,23 +1,11 @@
 require "spec_helper"
 
 class Song
-  attr_accessor :name
+  attr_accessor :name, :artist
 
-  def initialize(name)
+  def initialize(name, artist)
     @name = name
-  end
-
-  it "belongs to an artist" do
-    artist = Artist.new
-    song.artist = artist
-    expect(song.artist).to eq(artist)
-  end
-
-  it "knows the name of the artist it belongs to" do
-    artist = Artist.new
-    artist.name = "Beyonce"
-    song.artist = artist
-    expect(song.artist.name).to eq("Beyonce")
+    @artist = artist
   end
 
 end
