5ecf0616a5e26b7225cfd22e114b6d893bb6306b
Keillan Cruickshank <cruickshankxkeillan90@gmail.com>
2019-10-31 13:45:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,15 +2,6 @@ require "spec_helper"
 
 class Song
 
-  attr_accessor :name, :artist
-
-  def initialize(name)
-    @name = name
-    name = self.new
-  end
-
-  def artist
-    self.artist
-  end
+  attr_accessor :title, :artist
 
 end
